af30ff0149f9f91b0cc93cfc4e6a1459727df1b3
balasahebjavheri-devops <balasahebjavheri@gmail.com>
2025-10-29 01:16:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-01361d3186814b895"
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "balasaheb-first-keypair"
   vpc_security_group_ids = ["sg-054cf71124f368820"]
   tags = {
     Name = var.instance_names[count.index]
